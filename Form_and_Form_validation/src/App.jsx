e858b3d8ccfd0e8c7b9690bbc72166b4177f56d3
Ankit Kumar <ankitiit263@gmail.com>
2024-05-25 12:27:52 +0530

    first commit

---
+++ Form_and_Form_validation/src/App.jsx
@@ -0,0 +1,18 @@
+// // src/App.jsx
+
+// import React from 'react';
+// // import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import Form from './Form';
+// import Success from './Success';
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <Route exact path="/" component={Form} />
+//       <Route path="/success" component={Success} />
+//     </Router>
+//   );
+// };
+
+// export default App;
